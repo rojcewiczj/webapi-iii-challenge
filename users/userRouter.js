29ec0a4271b2edc16ef68587abd991a046d7b58c
John Rojcewicz <rojcewiczj@gmail.com>
2019-10-02 15:33:28 -0500

    working through middleware

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,5 +1,5 @@
 const express = 'express';
-
+const Users = require('./userDb')
 const router = express.Router();
 
 router.post('/', (req, res) => {
@@ -33,12 +33,43 @@ router.put('/:id', (req, res) => {
 //custom middleware
 
 function validateUserId(req, res, next) {
-
+    const { id } = req.params;
+    Users.findById(id)
+    .then(user => {
+      if (user) {
+        req.user = user;
+        next();
+      } else {
+        res.status(400).json({ message: "Invalid user ID"});
+        
+        // error handling middleware option:
+        // next({ message: "Invalid id; hub not found"});
+      }
+    })
+    .catch(error => {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: 'Error processing request',
+      });
+    });
 };
 
 function validateUser(req, res, next) {
-
-};
+    const User = req.body;
+    if (User && Object.keys(User).length > 0) {
+        next();
+      } else if (!User.name ){
+        res.status(400).json({ message: 'missing required name field' });
+      }
+      else {
+        res.status(400).json({ message: "missing user data" });
+      
+        // error handling middleware option:
+        // next({ message: "Please include request body" }));
+      }
+    };
+      
 
 function validatePost(req, res, next) {
 
