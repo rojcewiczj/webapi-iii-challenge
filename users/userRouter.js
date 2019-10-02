b95cad6eac99f481f832ab1895f63ee8dcd74ee2
John Rojcewicz <rojcewiczj@gmail.com>
2019-10-02 15:49:52 -0500

    middleware built out

--- users/userRouter.js
+++ users/userRouter.js
@@ -31,7 +31,7 @@ router.put('/:id', (req, res) => {
 });
 
 //custom middleware
-
+function logger()
 function validateUserId(req, res, next) {
     const { id } = req.params;
     Users.findById(id)
@@ -55,6 +55,16 @@ function validateUserId(req, res, next) {
     });
 };
 
+function logger(req, res, next) {
+    console.log(
+      `[${new Date().toISOString()}] ${req.method} to ${req.url} `
+    );
+  
+    next();
+  }
+
+
+
 function validateUser(req, res, next) {
     const User = req.body;
     if (User && Object.keys(User).length > 0) {
@@ -72,7 +82,19 @@ function validateUser(req, res, next) {
       
 
 function validatePost(req, res, next) {
-
+    const Post = req.body;
+    if (Post && Object.keys(Post).length > 0) {
+        next();
+      } else if (!Post.text ){
+        res.status(400).json({ message: 'missing required text field' });
+      }
+      else {
+        res.status(400).json({ message: "missing post data" });
+      
+        // error handling middleware option:
+        // next({ message: "Please include request body" }));
+      }
+    
 };
 
 module.exports = router;
